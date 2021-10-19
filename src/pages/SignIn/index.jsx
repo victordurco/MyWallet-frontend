3f25da4998dff14af02503cf57f4e8fcf0307403
victordurco <victordurco@gmail.com>
2021-10-19 14:05:50 -0300

    SignIn page layout done

---
+++ src/pages/SignIn/index.jsx
@@ -0,0 +1,86 @@
+import { useState } from "react";
+import styled from "styled-components";
+import logo from "../../img/myWallet.png";
+
+export default function SignIn() {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const login = (event) => {
+        event.preventDefault();
+    };
+
+    return (
+        <Background>
+            <Wrapper onSubmit={login}>
+                <Logo src={logo} />
+                <Input
+                    type="email"
+                    placeholder="E-mail"
+                    onChange={(e) => setEmail(e.target.value)}
+                    value={email}
+                />
+                <Input
+                    type="password"
+                    placeholder="Senha"
+                    onChange={(e) => setPassword(e.target.value)}
+                    value={password}
+                />
+                <Button type="submit">Entrar</Button>
+            </Wrapper>
+        </Background>
+    );
+}
+
+const Background = styled.div`
+    width: 100%;
+    height: 100vh;
+    background-color: #8c11be;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`;
+
+const Logo = styled.img`
+    width: 147px;
+    height: 40px;
+    margin-bottom: 24px;
+`;
+
+const Wrapper = styled.form`
+    width: 87%;
+    background-color: inherit;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+`;
+
+const Input = styled.input`
+    width: 100%;
+    height: 58px;
+    border: none;
+    border-radius: 5px;
+    margin-bottom: 13px;
+    padding: 17px 15px 17px 15px;
+    font-size: 20px;
+
+    ::placeholder {
+        font-family: "Raleway", sans-serif;
+        font-weight: 400;
+        font-size: 20px;
+        color: black;
+    }
+`;
+
+const Button = styled.button`
+    width: 100%;
+    height: 46px;
+    border: none;
+    border-radius: 5px;
+    background-color: #a328d6;
+    color: white;
+    font-weight: 700;
+    font-size: 20px;
+    font-family: "Raleway", sans-serif;
+`;
